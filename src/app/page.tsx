36c601be7f89875bdf1ab89879da47e5159656b6
vzsoares <vinicius.zenha@gmail.com>
2025-07-12 12:09:49 -0300

    feat: section container class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 font-bold text-4xl text-blue-600'>
-      Hello, world!
-    </main>
-  )
+    return (
+        <main className='flex min-h-screen flex-col'>
+            <section className="section-container">Hello, world!</section>
+        </main>
+    )
 }
