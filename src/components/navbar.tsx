ed9b0c1a641f089fed788c2eec9a142aad6d6b48
vzsoares <vinicius.zenha@gmail.com>
2025-07-12 18:20:26 -0300

    feat: navbar styled

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <section className="bg-gray-200 box">
             <div className="section-container py-4">
-                <div className="flex justify-between"><h3>GamerShop</h3><div>cart icon</div></div>
+                <div className="flex justify-between">
+                    <h3 className="text-background text-2xl font-bold">GamerShop</h3>
+                    <Link href="/cart" className="">
+                        <Image src="/icons/cart-icon.svg" width={24} height={24} alt="cart icon" style={{ cursor: 'pointer' }} />
+                    </Link>
+                </div>
             </div>
         </section>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+        colors: {
+          'background': '#585660',
+        },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
