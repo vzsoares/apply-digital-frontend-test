1feca12c0af6ad45477ea2392a9e9dc68144ca1d
vzsoares <vinicius.zenha@gmail.com>
2025-07-12 18:26:52 -0300

    feat: footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <section className="bg-gray-200 box">
+        <section className="bg-[#404040] box">
             <div className="section-container py-8">
-                <div className="self-center">apply icon</div>
+                <div className="self-center" >
+                    <Link href="/">
+                        <Image src="/images/apply-digital-logo.png" alt="apply-digital-logo" width={170} height={44} />
+                    </Link>
+                </div>
             </div>
-        </section>
+        </section >
     )
 }
 
