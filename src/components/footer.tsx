a8dad6882bb14972b1e47e010f022b0e1ecc6f69
vzsoares <vinicius.zenha@gmail.com>
2025-07-12 12:54:03 -0300

    feat: navbar and footer components

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <section className="bg-gray-200 box">
+            <div className="section-container py-8">
+                <div className="self-center">apply icon</div>
+            </div>
+        </section>
+    )
+}
+
---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <section className="bg-gray-200 box">
+            <div className="section-container py-4">
+                <div className="flex justify-between"><h3>GamerShop</h3><div>cart icon</div></div>
+            </div>
+        </section>
+
+    )
+}
+
