ef0fc32248aa51ef8236c1d4fe8ecae50b97e9ec
vzsoares <vinicius.zenha@gmail.com>
2025-07-14 23:35:58 -0300

    feat: navbar route

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,9 @@ export default function Navbar() {
         <section className="bg-gray-200 box">
             <div className="section-container py-4">
                 <div className="flex justify-between">
-                    <h3 className="text-background text-2xl font-bold">GamerShop</h3>
+                    <Link href="/">
+                        <h3 className="text-background text-2xl font-bold">GamerShop</h3>
+                    </Link>
                     <Link href="/cart" className="relative">
                         <Image src="/icons/cart-icon.svg" width={24} height={24} alt="cart icon" style={{ cursor: 'pointer' }} />
                         {itemCount > 0 && (
