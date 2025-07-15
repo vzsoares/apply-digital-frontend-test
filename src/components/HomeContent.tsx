c235c11795d2d8fcf47e934ececb0f385c86d152
vzsoares <vinicius.zenha@gmail.com>
2025-07-15 11:59:23 -0300

    fix: layout shift

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 
 export default function HomeContent() {
     return (
-        <Suspense fallback={<div className="w-full h-full flex justify-center items-center">Loading...</div>}>
+        <Suspense fallback={<div className="w-full h-full flex justify-center items-center flex-1">Loading...</div>}>
             {/* Search section */}
             <SearchSection />
             {/* Card section */}
