438f7ae54045f1fe4f254cf10686fdd09d6422ed
vzsoares <vinicius.zenha@gmail.com>
2025-07-14 21:20:06 -0300

    feat: api response type

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,31 +1,39 @@
 import { allGames, availableFilters, delay } from "@/utils/endpoint";
+import type { Game } from "@/utils/endpoint";
 
 const ITEMS_PER_PAGE = 12;
 
+export interface GamesApiResponse {
+    games: Game[];
+    availableFilters: string[];
+    totalPages: number;
+    currentPage: number;
+}
+
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const genre = searchParams.get("genre");
-  let page = parseInt(searchParams.get("page") ?? "1");
+    const { searchParams } = new URL(request.url);
+    const genre = searchParams.get("genre");
+    let page = parseInt(searchParams.get("page") ?? "1");
 
-  let games = allGames;
+    let games = allGames;
 
-  if (genre) {
-    games = games.filter(
-      (game) => game.genre.toLowerCase() === genre.toLowerCase()
-    );
-  }
+    if (genre) {
+        games = games.filter(
+            (game) => game.genre.toLowerCase() === genre.toLowerCase()
+        );
+    }
 
-  if (page < 1 || isNaN(page)) page = 1;
+    if (page < 1 || isNaN(page)) page = 1;
 
-  // Mock a delay to simulate a real API
-  await delay(2000);
+    // Mock a delay to simulate a real API
+    await delay(2000);
 
-  const fromIndex = (page - 1) * ITEMS_PER_PAGE;
-  const toIndex = page * ITEMS_PER_PAGE;
-  games = games.slice(fromIndex, toIndex);
+    const fromIndex = (page - 1) * ITEMS_PER_PAGE;
+    const toIndex = page * ITEMS_PER_PAGE;
+    games = games.slice(fromIndex, toIndex);
 
-  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
-  const currentPage = page;
+    const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
+    const currentPage = page;
 
-  return Response.json({ games, availableFilters, totalPages, currentPage });
+    return Response.json({ games, availableFilters, totalPages, currentPage });
 }
